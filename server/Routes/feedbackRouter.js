bce6c2ff054060431ca18afdc8b50beb0b874868
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-08-23 21:23:25 -0500

    adds POST route

--- server/Routes/feedbackRouter.js
+++ server/Routes/feedbackRouter.js
@@ -14,3 +14,21 @@ router.get('/', (req, res) => {
     res.sendStatus(500);
   })
 })
+
+//POST feedback to database
+router.post('/', (req, res) => {
+  console.log('in POST', req.body);
+  let feedback = req.body;
+  let queryText = `INSERT INTO "feedback"("feeling", "understanding", "support", "comments")
+  VALUES($1, $2, $3, $4);`;
+  pool.query(queryText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comments])
+  .then((result) => {
+    console.log('POST successful');
+    res.sendStatus(201);
+  }).catch((error) => {
+    console.log('POST error', error);
+    res.sendStatus(500);
+  })
+})
+
+module.exports = router;
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,33 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import Axios from 'axios';
 
 class Review extends Component {
-  state = { 
 
-   }
-  render() { 
-    return ( 
+  handleClick = () => {
+    Axios.post('/feedback', this.props.reduxStore)
+      .then((response) => {
+        console.log(response);
+
+      })
+      .catch((error) => {
+        console.log(error);
+
+      })
+  }
+
+  render() {
+    return (
       <>
-      <h1>Review of your Feedback</h1>
-      <ul>
-        <li>How you are feeling overall: {this.props.feeling}</li>
-        <li>Your understanding of this week: {this.props.understanding}</li>
-        <li>How well you feel supported: {this.props.support}</li>
-        <li>Comments: {this.props.comments}</li>
-      </ul>
-      <button>Submit</button>
+        <h1>Review of your Feedback</h1>
+        <ul>
+          <li>How you are feeling overall: {this.props.feeling}</li>
+          <li>Your understanding of this week: {this.props.understanding}</li>
+          <li>How well you feel supported: {this.props.support}</li>
+          <li>Comments: {this.props.comments}</li>
+        </ul>
+        <button onClick={this.handleClick}>Submit</button>
       </>
-     );
+    );
   }
 }
 
 const storeToProps = (reduxStore) => {
   return {
+    reduxStore,
     feeling: reduxStore.feeling,
     understanding: reduxStore.understanding,
     support: reduxStore.support,
     comments: reduxStore.comments
   }
 }
- 
+
 export default connect(storeToProps)(Review);
